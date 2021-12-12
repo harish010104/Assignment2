3a9ba98d1f553e9584ff38dabb09a712a9edde72
harish010104 <91388258+harish010104@users.noreply.github.com>
2021-12-12 22:16:54 +0530

    Add files via upload

---
+++ Assignment-2.py
@@ -0,0 +1,16 @@
+n=5;
+for i in range(n):
+    for j in range(i):
+        print ('* ', end="")
+    print('')
+
+for i in range(n,0,-1):
+    for j in range(i):
+        print('* ', end="")
+    print('')
+    #questiom(2a)
+    word = input("Input a word to reverse: ")
+ 
+for char in range(len(word) - 1, -1, -1):
+  print(word[char], end="")
+print("\n")
